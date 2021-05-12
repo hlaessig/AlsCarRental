19e2c69a46cf3c03b6fcc2f6a546f2c846e90db9
Johannes Laessig <laessig.johannes@gmail.com>
2021-05-12 00:10:13 -0400

    CONNECT GUI

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+from datetime import date
+import textwrap
+
 import mysql.connector
 from mysql.connector import Error
 import PySimpleGUI as sg
+
+# -- Constants --#
+today = date.today()
+today_format = today.strftime("%B %d, %Y")
 
 
 # -- database connections -- #
@@ -47,7 +54,6 @@ def execute_query(connection, query):
 
 def read_query(connection, query):
     cursor = connection.cursor()
-    result = None
     try:
         cursor.execute(query)
         result = cursor.fetchall()
@@ -56,8 +62,18 @@ def read_query(connection, query):
         print(f"Error: '{err}'")
 
 
+def execute_list_query(connection, sql, val):
+    cursor = connection.cursor()
+    try:
+        cursor.executemany(sql, val)
+        connection.commit()
+        print("Query successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+
 # -- GUI --#
-def customer_window():
+def dashboard_main(connection):
     theme_dict = {'BACKGROUND': '#2B475D',
                   'TEXT': '#FFFFFF',
                   'INPUT': '#F2EFE8',
@@ -77,26 +93,34 @@ def customer_window():
     BPAD_LEFT_INSIDE = (0, 10)
     BPAD_RIGHT = ((10, 20), (10, 20))
 
-    top_banner = [[sg.Text('Dashboard' + ' ' * 64, font='Any 20', background_color=DARK_HEADER_COLOR),
-                   sg.Text('Tuesday 9 June 2020', font='Any 20', background_color=DARK_HEADER_COLOR)]]
+    top_banner = [[sg.Text('Al Rental' + ' ' * 64, font='Any 20', background_color=DARK_HEADER_COLOR),
+                   sg.Text(today_format, font='Any 20', background_color=DARK_HEADER_COLOR)]]
 
-    block_2 = [[sg.Text('Customers', font='Any 20')],
-               [sg.Button('Add New'), sg.Button('Update'), sg.Button('Delete'), sg.Button('Get')]]
+    block_2 = [[sg.Text('Lookup', font='Any 20')],
+               [sg.Text('Customer ID', size=(10, 1)), sg.InputText(key='-LOOKUP_ID-')],
+               [sg.Button('Get')]]
 
-    block_3 = [[sg.Text('Please enter your Name, Address, Phone')],
-               [sg.Text('Name', size=(10, 1)), sg.InputText(key='-NAME-')],
-               [sg.Text('Address', size=(10, 1)), sg.InputText(key='-ADDRESS-')],
+    block_3 = [[sg.Text('Update Customer', font='Any 20')],
+               [sg.Text('First Name', size=(10, 1)), sg.InputText(key='-FIRSTNAME-')],
+               [sg.Text('Last Name', size=(10, 1)), sg.InputText(key='-LASTNAME-')],
                [sg.Text('Phone', size=(10, 1)), sg.InputText(key='-PHONE-')],
-               [sg.Button('Submit'), sg.Button('Exit')]]
+               [sg.Text('Email', size=(10, 1)), sg.InputText(key='-EMAIL-')],
+               [sg.Text('Credit Card', size=(10, 1)), sg.InputText(key='-CREDIT_CARD-')],
+               [sg.Button('Update'), sg.Button('Delete')]]
 
-    block_4 = [[sg.Text('Block 4', font='Any 20')],
-               [sg.T('make a selection')]]
+    block_4 = [[sg.Text('Add Customers', font='Any 20')],
+               [sg.Text('First Name', size=(10, 1)), sg.InputText(key='-NEW_FIRSTNAME-')],
+               [sg.Text('Last Name', size=(10, 1)), sg.InputText(key='-NEW_LASTNAME-')],
+               [sg.Text('Phone', size=(10, 1)), sg.InputText(key='-NEW_PHONE-')],
+               [sg.Text('Email', size=(10, 1)), sg.InputText(key='-NEW_EMAIL-')],
+               [sg.Text('Credit Card', size=(10, 1)), sg.InputText(key='-NEW_CREDIT_CARD-')],
+               [sg.Button('Add User'), sg.Button('Clear'), sg.Button('Exit')]]
 
     layout = [[sg.Column(top_banner, size=(960, 60), pad=(0, 0), background_color=DARK_HEADER_COLOR)],
               [sg.Column([[sg.Column(block_2, size=(450, 150), pad=BPAD_LEFT_INSIDE)],
-                          [sg.Column(block_3, size=(450, 150), pad=BPAD_LEFT_INSIDE)]], pad=BPAD_LEFT,
+                          [sg.Column(block_3, size=(450, 200), pad=BPAD_LEFT_INSIDE)]], pad=BPAD_LEFT,
                          background_color=BORDER_COLOR),
-               sg.Column(block_4, size=(450, 320), pad=BPAD_RIGHT)]]
+               sg.Column(block_4, size=(450, 370), pad=BPAD_RIGHT)]]
 
     window = sg.Window('Dashboard PySimpleGUI-Style', layout, margins=(0, 0), background_color=BORDER_COLOR,
                        no_titlebar=True, grab_anywhere=True)
@@ -105,7 +129,27 @@ def customer_window():
         event, values = window.read()
 
         if event == 'Get':
-            customer_window_get()
+            get_id = values['-LOOKUP_ID-']
+            dashboard_get_customer(connection, get_id)
+            break
+
+        if event == 'Clear':
+            dashboard_main(connection)
+            break
+
+        if event == 'Add':
+            sql = '''
+                INSERT INTO teacher (first_name, last_name, phone_no, email, credit_card) 
+                VALUES (%s, %s, %s, %s, %s)
+                '''
+
+            val = [
+                (values['-NEW_FIRSTNAME-']), (values['-NEW_LASTNAME-']), (values['-NEW_PHONE-']),
+                (values['-NEW_EMAIL-']), (values['-NEW_CREDIT_CARD-'])]
+
+            execute_list_query(connection, sql, val)
+
+            dashboard_main(connection)
             break
 
         if event == sg.WIN_CLOSED or event == 'Exit':
@@ -113,15 +157,21 @@ def customer_window():
     window.close()
 
 
-def customer_window_get():
-    # -- Establish customer DB connection -- #
-    q1 = """
-               SELECT *
-               FROM customer;
-               """
-    connection = create_db_connection("localhost", "root", 'mypassword', 'al_rentals')
+def dashboard_get_customer(connection, get_id):
+    global phone, email, customer_id, first_name, last_name, credit_card
+    q1 = "SELECT * FROM customer WHERE customer_id = '%s'" % get_id
+    print(q1)
+
     results = read_query(connection, q1)
 
+    for row in results:
+        customer_id = row[0]
+        first_name = row[1]
+        last_name = row[2]
+        phone = row[3]
+        email = row[4]
+        credit_card = row[5]
+
     theme_dict = {'BACKGROUND': '#2B475D',
                   'TEXT': '#FFFFFF',
                   'INPUT': '#F2EFE8',
@@ -141,26 +191,34 @@ def customer_window_get():
     BPAD_LEFT_INSIDE = (0, 10)
     BPAD_RIGHT = ((10, 20), (10, 20))
 
-    top_banner = [[sg.Text('Dashboard' + ' ' * 64, font='Any 20', background_color=DARK_HEADER_COLOR),
-                   sg.Text('Tuesday 9 June 2020', font='Any 20', background_color=DARK_HEADER_COLOR)]]
+    top_banner = [[sg.Text('Al Rental' + ' ' * 64, font='Any 20', background_color=DARK_HEADER_COLOR),
+                   sg.Text(today_format, font='Any 20', background_color=DARK_HEADER_COLOR)]]
 
-    block_2 = [[sg.Text('Customers', font='Any 20')],
-               [sg.Button('Add New'), sg.Button('Update'), sg.Button('Delete'), sg.Button('Get')]]
+    block_2 = [[sg.Text('Lookup', font='Any 20')],
+               [sg.Text('Customer ID', size=(10, 1)), sg.InputText(customer_id, key='-LOOKUP_ID-')],
+               [sg.Button('Get')]]
 
-    block_3 = [[sg.Text('Please enter your Name, Address, Phone')],
-               [sg.Text('Name', size=(10, 1)), sg.InputText(key='-NAME-')],
-               [sg.Text('Address', size=(10, 1)), sg.InputText(key='-ADDRESS-')],
-               [sg.Text('Phone', size=(10, 1)), sg.InputText(key='-PHONE-')],
-               [sg.Button('Submit'), sg.Button('Exit')]]
+    block_3 = [[sg.Text('Update Customer', font='Any 20')],
+               [sg.Text('First Name', size=(10, 1)), sg.InputText(first_name, key='-FIRSTNAME-')],
+               [sg.Text('Last Name', size=(10, 1)), sg.InputText(last_name, key='-LASTNAME-')],
+               [sg.Text('Phone', size=(10, 1)), sg.InputText(phone, key='-PHONE-')],
+               [sg.Text('Email', size=(10, 1)), sg.InputText(email, key='-EMAIL-')],
+               [sg.Text('Credit Card', size=(10, 1)), sg.InputText(credit_card, key='-CREDIT_CARD-')],
+               [sg.Button('Update'), sg.Button('Delete')]]
 
-    block_4 = [[sg.Text('Block 4', font='Any 20')],
-               [sg.T(results)]]
+    block_4 = [[sg.Text('Add Customers', font='Any 20')],
+               [sg.Text('First Name', size=(10, 1)), sg.InputText(key='-NEW_FIRSTNAME-')],
+               [sg.Text('Last Name', size=(10, 1)), sg.InputText(key='-NEW_LASTNAME-')],
+               [sg.Text('Phone', size=(10, 1)), sg.InputText(key='-NEW_PHONE-')],
+               [sg.Text('Email', size=(10, 1)), sg.InputText(key='-NEW_EMAIL-')],
+               [sg.Text('Credit Card', size=(10, 1)), sg.InputText(key='-NEW_CREDIT_CARD-')],
+               [sg.Button('Add User'), sg.Button('Clear'), sg.Button('Exit')]]
 
     layout = [[sg.Column(top_banner, size=(960, 60), pad=(0, 0), background_color=DARK_HEADER_COLOR)],
               [sg.Column([[sg.Column(block_2, size=(450, 150), pad=BPAD_LEFT_INSIDE)],
-                          [sg.Column(block_3, size=(450, 150), pad=BPAD_LEFT_INSIDE)]], pad=BPAD_LEFT,
+                          [sg.Column(block_3, size=(450, 200), pad=BPAD_LEFT_INSIDE)]], pad=BPAD_LEFT,
                          background_color=BORDER_COLOR),
-               sg.Column(block_4, size=(450, 320), pad=BPAD_RIGHT)]]
+               sg.Column(block_4, size=(450, 370), pad=BPAD_RIGHT)]]
 
     window = sg.Window('Dashboard PySimpleGUI-Style', layout, margins=(0, 0), background_color=BORDER_COLOR,
                        no_titlebar=True, grab_anywhere=True)
@@ -169,14 +227,57 @@ def customer_window_get():
         event, values = window.read()
 
         if event == 'Get':
-            q1 = """
-            SELECT *
-            FROM customer;
-            """
-            results = read_query(connection, q1)
+            get_id = values['-LOOKUP_ID-']
+            dashboard_get_customer(connection, get_id)
+            break
+
+        if event == 'Clear':
+            dashboard_main(connection)
+            break
+
+        if event == 'Delete':
+            delete_course = '''
+            DELETE FROM customer
+            WHERE customer_id = '%s';
+            ''' % get_id
+
+            confirm_Delete(get_id, connection, delete_course)
+            break
+
+        if event == 'Update':
+            update = '''
+            UPDATE customer 
+            SET 
+            first_name = '%s', last_name = '%s', phone_no = '%s', email = '%s', credit_card = '%s' 
+            WHERE customer_id = %s;
+            ''' % (values['-FIRSTNAME-'], values['-LASTNAME-'], values['-PHONE-'],
+                   values['-EMAIL-'], values['-CREDIT_CARD-'], get_id)
+            print(update)
+            execute_query(connection, update)
+            dashboard_get_customer(connection, get_id)
+            break
+
         if event == sg.WIN_CLOSED or event == 'Exit':
             break
     window.close()
 
 
-customer_window()
+def confirm_Delete(customer_id, connection, delete):
+    window = sg.Window('Confirm Action',
+                       [[sg.Text('Are you sure you want to delete %s' % customer_id)],
+                        [sg.OK(), sg.Cancel()]])
+    event, values = window.read()
+
+    if event != 'OK':
+        dashboard_get_customer(connection, customer_id)
+        window.close()
+
+    else:
+        execute_query(connection, delete)
+        dashboard_main(connection)
+        window.close()
+
+
+# -- MAIN -- #
+connection = create_db_connection("localhost", "root", 'mypassword', 'al_rentals')
+dashboard_main(connection)
